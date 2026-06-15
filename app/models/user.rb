ea9da88c52a0f66580d298a82accf0bc6121f68b
Manish10022001 <manishshirsat2850@gmail.com>
2026-06-16 00:23:27 +0530

    Created user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+# email : string
+# password_digest : string
+
+#has_secure_password along with above fields add 2 virtual attribues, and we r going to interact with these two only instead of password_digest
+# password : string virtual
+# password_confirmation : string virtual
+
+#has_secure_password make sure password and password_confirmation match, then it takes the password and run it through bcrypt to create a digest(hash) and then assign that to password_digest
 class User < ApplicationRecord
-    has_secured_password
+    has_secure_password
 end
