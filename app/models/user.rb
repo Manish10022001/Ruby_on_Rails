758dcad5a63d0d733469a2034852f8e79895f4a6
Manish10022001 <manishshirsat2850@gmail.com>
2026-06-16 01:16:14 +0530

    Validations from email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@
 #has_secure_password make sure password and password_confirmation match, then it takes the password and run it through bcrypt to create a digest(hash) and then assign that to password_digest
 class User < ApplicationRecord
     has_secure_password
+
+    #validation using ruby //this is hash 
+    validates :email, presence: true, format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message:"Must be valid email address"}
 end
